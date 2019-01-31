c05e4e9a2642198794e13156b975bd0f7a3fb64c
GaseousClay <GaseousClay@users.noreply.github.com>
2019-01-31 14:56:51 -0600

    Cleaning things up

--- final+model.R
+++
@@ -1,6 +0,0 @@
-giftdata <- import("110-115Congresses.csv")
-giftdata$democrat <- ifelse(giftdata$party == 100, 1, 0)
-finalmodel <- glm.nb(trips ~ extreme*democrat + winpct + seniority + chairman + committee + party.leader + fifteenth + fourteenth + thirteenth + twelfth + eleventh, data = giftdata)
-summary(finalmodel)
-
-
